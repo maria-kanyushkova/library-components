889753cbc93d81cd26c1eb7d65bfda4e80371bbc
Maria Kanyushkova <mashakanyushkova@gmail.com>
2019-08-08 17:57:46 +0300

    [issue-#1] add page with typography

--- src/pages/typography/TypographyPage.tsx
+++ src/pages/typography/TypographyPage.tsx
@@ -1,12 +1,90 @@
 import { Component, default as React, ReactNode } from "react";
+import "./Typography.scss";
 
 export class TypographyPage extends Component {
     render(): ReactNode {
         return (
-            <div className="components-store">
-                <div className="components-store_4-column">
-                    <div className="components-store__item">
+            <div className="components-store typography-store">
+                <div className="components-store_2-column">
+                    <div className="typography" data-type="SFDisplay-Thin">
+                        <span>SFDisplay-Thin</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
                     </div>
+                    <div />
+                </div>
+                <div className="components-store_2-column">
+                    <div className="typography" data-type="SFDisplay-UltraLight">
+                        <span>SFDisplay-UltraLight</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                    <div />
+                </div>
+                <div className="components-store_2-column">
+                    <div className="typography" data-type="SFDisplay-Light">
+                        <span>SFDisplay-Light</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                    <div className="typography" data-type="SFText-Light">
+                        <span>SFText-Light</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                </div>
+                <div className="components-store_2-column">
+                    <div className="typography" data-type="SFDisplay-Regular">
+                        <span>SFDisplay-Regular</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                    <div className="typography" data-type="SFText-Regular">
+                        <span>SFText-Regular</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                </div>
+                <div className="components-store_2-column">
+                    <div className="typography" data-type="SFDisplay-Medium">
+                        <span>SFDisplay-Medium</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                    <div className="typography" data-type="SFText-Medium">
+                        <span>SFText-Medium</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                </div>
+                <div className="components-store_2-column">
+                    <div className="typography" data-type="SFDisplay-Semibold">
+                        <span>SFDisplay-Semibold</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                    <div className="typography" data-type="SFText-Semibold">
+                        <span>SFText-Semibold</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                </div>
+                <div className="components-store_2-column">
+                    <div className="typography" data-type="SFDisplay-Bold">
+                        <span>SFDisplay-Bold</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                    <div className="typography" data-type="SFText-Bold">
+                        <span>SFText-Bold</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                </div>
+                <div className="components-store_2-column">
+                    <div className="typography" data-type="SFDisplay-Heavy">
+                        <span>SFDisplay-Heavy</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                    <div className="typography" data-type="SFText-Heavy">
+                        <span>SFText-Heavy</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                </div>
+                <div className="components-store_2-column">
+                    <div className="typography" data-type="SFDisplay-Black">
+                        <span>SFDisplay-Black</span>
+                        <span>Эй, жлоб! Где туз? Прячь юных съёмщиц в шкаф.</span>
+                    </div>
+                    <div />
                 </div>
             </div>
         );
