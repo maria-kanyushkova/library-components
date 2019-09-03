98a2df9e85568baa22b5597e06f2a390369ce596
Maria Kanyushkova <mashakanyushkova@gmail.com>
2019-09-03 10:20:39 +0300

    fix [TASS-126] заменены табы на пробелы, поправлен интерфейс в Modals

--- src/components/tab/Tab.tsx
+++ src/components/tab/Tab.tsx
@@ -5,45 +5,45 @@ import { ITabItem } from ".";
 import classNames from "classnames";
 
 export interface ICustomTabProps {
-	items: ITabItem[];
-	className?: string;
-	type?: string;
+    items: ITabItem[];
+    className?: string;
+    type?: string;
 }
 
 export const Tab: FC<ICustomTabProps> = (props) => {
-	const {children, className = "", items} = props;
-	const classes = classNames({
-		["tabs-container"]: true,
-		[className]: true,
-	});
-	const [activeTab, setActiveTab] = useState(0);
-	return (
-		<div className={classes}>
-			<div className="tabs">
-				{
-					items.map((item: ITabItem, index) => {
-						const {text, handler} = item;
-						const isActiveTab = activeTab === index;
-						const onClick = () => {
-							setActiveTab(index);
-							handler();
-						};
-						return (
-							<div
-								key={index}
-								className="tab-item__label"
-								data-active={isActiveTab}
-								onClick={onClick}
-							>
-								{text}
-							</div>
-						);
-					})
-				}
-			</div>
-			<div className="tabs-item tab-content">
-				{children}
-			</div>
-		</div>
-	);
+    const { children, className = "", items } = props;
+    const classes = classNames({
+        ["tabs-container"]: true,
+        [className]: true,
+    });
+    const [ activeTab, setActiveTab ] = useState(0);
+    return (
+        <div className={ classes }>
+            <div className="tabs">
+                {
+                    items.map((item: ITabItem, index) => {
+                        const { text, handler } = item;
+                        const isActiveTab = activeTab === index;
+                        const onClick = () => {
+                            setActiveTab(index);
+                            handler();
+                        };
+                        return (
+                            <div
+                                key={ index }
+                                className="tab-item__label"
+                                data-active={ isActiveTab }
+                                onClick={ onClick }
+                            >
+                                { text }
+                            </div>
+                        );
+                    })
+                }
+            </div>
+            <div className="tabs-item tab-content">
+                { children }
+            </div>
+        </div>
+    );
 };
